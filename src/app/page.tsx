8706db44f9a2660c3ce375a519013f49526de244
Longbo <longboqiu@hotmail.com>
2024-07-24 11:04:21 +0200

    WIP: 79fe757 feat: add first version of header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/header";
+import { Hero } from "@/components/hero";
 
 export default function Home(): JSX.Element {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between">
+        <main className="flex min-h-screen flex-col items-center">
             <Header />
-        </div>
+            <Hero />
+        </main>
     );
 }
