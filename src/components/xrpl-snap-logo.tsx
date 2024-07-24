d7a54fc32d7e639acb4b05c691d33ce5b1162141
Longbo <longboqiu@hotmail.com>
2024-07-24 18:31:44 +0200

    fix: svg typo

--- src/components/xrpl-snap-logo.tsx
+++ src/components/xrpl-snap-logo.tsx
@@ -1,41 +1,41 @@
 export function XRPLSnapLogo(): JSX.Element {
     return (
         <svg width="414" height="384" viewBox="0 0 414 384" fill="none" xmlns="http://www.w3.org/2000/svg" className="z-10">
-            <g clip-path="url(#clip0_146_1818)">
+            <g clipPath="url(#clip0_146_1818)">
                 <path
                     d="M391.608 0.81189L231.324 120.802L260.965 50.0093L391.608 0.81189Z"
                     fill="#8432D5"
                     stroke="#8432D5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M21.5863 0.81189L180.582 121.938L152.391 50.0093L21.5863 0.81189ZM333.939 278.948L291.25 344.869L382.588 370.199L408.845 280.409L333.939 278.948ZM4.67188 280.409L30.7684 370.199L122.106 344.869L79.4174 278.948L4.67188 280.409Z"
                     fill="#8432D5"
                     stroke="#8432D5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M116.95 167.564L91.498 206.37L182.191 210.429L178.97 112.196L116.95 167.564ZM296.243 167.564L233.418 111.06L231.324 210.429L321.856 206.37L296.243 167.564ZM122.105 344.869L176.553 318.079L129.515 281.059L122.105 344.869ZM236.64 318.079L291.249 344.869L283.678 281.059L236.64 318.079Z"
                     fill="#8432D5"
                     stroke="#8432D5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M291.251 344.869L236.642 318.079L240.991 353.962L240.508 369.062L291.251 344.869ZM122.107 344.869L172.851 369.062L172.528 353.962L176.556 318.079L122.107 344.869Z"
                     fill="#D7C1B3"
                     stroke="#D7C1B3"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M122.107 344.869L129.839 278.948L79.418 280.409L122.107 344.869ZM283.518 278.948L291.251 344.869L333.939 280.409L283.518 278.948ZM321.858 206.37L231.325 210.429L239.702 257.353L258.751 251.345L285.29 243.877L321.858 206.37ZM128.228 243.877L155.251 251.345L173.655 257.353L182.193 210.429L91.4997 206.37L128.228 243.877Z"
                     fill="#5E1BA1"
                     stroke="#5E1BA1"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M173.656 257.353L128.229 243.877L160.285 229.101L173.656 257.353ZM239.541 257.353L252.912 229.101L285.13 243.877L239.541 257.353Z"
@@ -45,43 +45,43 @@ export function XRPLSnapLogo(): JSX.Element {
                     d="M91.498 206.37L129.515 281.059L128.226 243.876L91.498 206.37ZM285.289 243.876L283.678 281.059L321.856 206.37L285.289 243.876ZM182.191 210.429L173.654 257.353L184.286 312.72L186.702 239.817L182.191 210.429ZM231.324 210.429L226.974 239.655L228.907 312.72L239.7 257.353L231.324 210.429Z"
                     fill="#8E45D6"
                     stroke="#8E45D6"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M239.702 257.353L228.909 312.721L236.642 318.079L283.68 281.059L285.291 243.877L239.702 257.353ZM128.229 243.877L129.517 281.059L176.555 318.079L184.288 312.721L173.656 257.353L128.229 243.877Z"
                     fill="#AC5CFB"
                     stroke="#AC5CFB"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M240.508 369.062L240.991 353.962L236.964 350.39H176.233L172.528 353.962L172.851 369.062L122.107 344.869L139.827 359.482L175.75 384.65H237.447L273.531 359.482L291.251 344.869L240.508 369.062Z"
                     fill="#C0AD9E"
                     stroke="#C0AD9E"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M236.641 318.079L228.909 312.721H184.287L176.555 318.079L172.527 353.962L176.232 350.39H236.963L240.99 353.962L236.641 318.079Z"
                     fill="#161616"
                     stroke="#161616"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M398.373 128.595L412.065 62.3493L391.607 0.81189L236.639 116.743L296.242 167.564L380.492 192.406L399.178 170.486L391.124 164.641L404.011 152.788L394.023 144.995L406.91 135.09L398.373 128.595ZM1.28711 62.3493L14.9797 128.595L6.28089 135.09L19.168 144.995L9.34158 152.788L22.2287 164.641L14.1743 170.486L32.6996 192.406L116.949 167.564L176.552 116.743L21.5844 0.81189L1.28711 62.3493Z"
                     fill="#500F90"
                     stroke="#500F90"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M380.494 192.406L296.244 167.564L321.857 206.369L283.679 281.059L333.939 280.409H408.845L380.494 192.406ZM116.951 167.564L32.7014 192.406L4.67188 280.409H79.4174L129.516 281.059L91.4991 206.369L116.951 167.564ZM238.225 210.429L257.341 116.742L261.126 50.0093H152.391L155.854 116.742L175.292 210.429L184.126 239.98L184.287 312.72H228.908L229.231 239.98L238.225 210.429Z"
                     fill="#AC5CFB"
                     stroke="#AC5CFB"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
                 <path
                     d="M236.274 132.655H248.249L223.326 107.548C214.302 98.4556 199.687 98.4556 190.663 107.548L165.75 132.655H177.725L196.656 113.58C202.277 107.888 211.471 107.818 217.183 113.42C217.233 113.47 217.293 113.53 217.343 113.58L236.274 132.655ZM177.574 62.8363H165.6L190.673 88.1029C199.697 97.1953 214.312 97.1953 223.336 88.1029L248.4 62.8363H236.425L217.343 82.0613C211.722 87.7528 202.528 87.8228 196.816 82.2214C196.766 82.1714 196.706 82.1114 196.656 82.0613L177.574 62.8363Z"
