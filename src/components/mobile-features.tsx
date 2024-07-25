d37adf7c5ab636955b1ff1f49027993b21e5dcb7
Longbo <longboqiu@hotmail.com>
2024-07-25 17:54:52 +0200

    feat: add carousel

---
+++ src/components/mobile-features.tsx
@@ -0,0 +1,50 @@
+import Autoplay from "embla-carousel-autoplay";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+
+export function MobileFeatures(): JSX.Element {
+    return (
+        <div>
+            <Carousel
+                className="w-full max-w-xs"
+                plugins={[
+                    Autoplay({
+                        delay: 3000,
+                    }),
+                ]}
+            >
+                <CarouselContent>
+                    <CarouselItem className="flex flex-col gap-10">
+                        <img src="feature.png" alt="feature-showcase" />
+
+                        <div>
+                            <p className="font-semibold text-lg text-white">XRPL in MetaMask</p>
+                            <p className="text-sm text-[#B0B0BA]">
+                                Easily create and manage an XRPL account in MetaMask with our wallet. Seamlessly connect, secure assets, and
+                                explore XRPL&apos;`s potential.
+                            </p>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="flex flex-col gap-10">
+                        <img src="feature.png" alt="feature-showcase" />
+
+                        <div>
+                            <p className="font-semibold text-lg text-white">Sign any transaction</p>
+                            <p className="text-sm text-[#B0B0BA]">Sign and send XRPL transactions with your Metamask</p>
+                        </div>
+                    </CarouselItem>
+                    <CarouselItem className="flex flex-col gap-10">
+                        <img src="feature.png" alt="feature-showcase" />
+
+                        <div>
+                            <p className="font-semibold text-lg text-white">Bridge your assets seamlessly to XRPL</p>
+                            <p className="text-sm text-[#B0B0BA]">
+                                Seamlessly bridge your assets to XRPL with our wallet and Metamask Snap integration. Securely transfer,
+                                manage, and explore new opportunities
+                            </p>
+                        </div>
+                    </CarouselItem>
+                </CarouselContent>
+            </Carousel>
+        </div>
+    );
+}
