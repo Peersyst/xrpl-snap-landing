e3d7363379a2a764d4bf62ed6644f9329e1519dd
Longbo <longboqiu@hotmail.com>
2024-07-29 11:14:15 +0200

    feat: add target blank to links

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -5,10 +5,10 @@ export function CTA(): JSX.Element {
     return (
         <div className="w-full bg-primary h-[528px] sm:h-[472px] flex justify-center items-center">
             <div className="max-w-4xl mx-auto flex flex-col gap-12 px-4 lg:px-0 items-center">
-                <p className="text-center font-semibold text-[44px]">Get started today, connect your Metamask</p>
+                <p className="text-center font-semibold text-[44px]">Get started today, connect your Metamask to XRPL</p>
 
                 <div>
-                    <Link href="https://xrpl-snap.vercel.app/" className={buttonVariants({ variant: "secondary" })}>
+                    <Link href="https://xrpl-snap.vercel.app/" target="_blank" className={buttonVariants({ variant: "secondary" })}>
                         Get started
                     </Link>
                 </div>
