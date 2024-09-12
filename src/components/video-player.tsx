302652a15c85240fef4278eb9f361b4710cc0915
Longbo <longboqiu@hotmail.com>
2024-09-12 10:46:41 +0200

    feat: add video thumbnail

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -12,16 +12,20 @@ export function VideoPlayer(): JSX.Element {
 
     return (
         <div className="relative w-full max-w-4xl mx-auto aspect-video rounded-2xl overflow-hidden">
-            <ReactPlayer url="https://www.youtube.com/watch?v=hG9gshvvZ8E" playing={playing} controls={false} width="100%" height="100%" />
-            {!playing && (
-                <Button
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-3"
-                    variant="secondary"
-                    onClick={() => setPlaying(true)}
-                >
-                    <PlayIcon className="h-5 w-5" />
-                    <span>Play video</span>
-                </Button>
+            {!playing ? (
+                <div onClick={() => setPlaying(true)} className="cursor-pointer">
+                    <img src="video-thumbnail.webp" className="w-full h-full" alt="video-thumbnail" />
+                    <Button
+                        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-3"
+                        variant="secondary"
+                        onClick={() => setPlaying(true)}
+                    >
+                        <PlayIcon className="h-5 w-5" />
+                        <span>Play video</span>
+                    </Button>
+                </div>
+            ) : (
+                <ReactPlayer url="https://www.youtube.com/watch?v=hG9gshvvZ8E" playing={playing} width="100%" height="100%" controls />
             )}
         </div>
     );
