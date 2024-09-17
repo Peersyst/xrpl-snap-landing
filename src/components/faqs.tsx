383a94aee73747de510e03cfa645b0d641518ead
vrc <vrivera@peersyst.com>
2024-09-17 17:34:22 +0200

    Add new entries to FAQs

--- src/components/faqs.tsx
+++ src/components/faqs.tsx
@@ -1,32 +1,122 @@
-const faqs: { question: string; answer: string }[] = [
+// Define the type correctly
+interface FAQ {
+    question: string;
+    answer: React.ReactNode;
+}
+
+const faqs: FAQ[] = [
     {
-        question: "What is MetaMask Snaps?",
-        answer: "MetaMask Snaps allows users to add features and functionality to their MetaMask wallet. Individual Snaps are features created by third-party developers that MetaMask users can install directly into their wallet.",
-    },
-    {
-        question: "How can I install MetaMask snap for XRPL?",
-        answer: "Simply click on Access Wallet and follow the instructions to install the Snap in your MetaMask. Once completed, you'll have the wallet connected to your MetaMask. Just make sure to have the MetaMask extension installed in your browser.",
-    },
-    { question: "Is it free?", answer: "Yes but keep in mind that network fees may apply." },
-    {
-        question: "How do I ensure I don't lose access to the wallet?",
-        answer: "As long as you have access to your MetaMask account or can recover it, you'll be able to access the XRPL wallet.",
-    },
+        question: "What are MetaMask Snaps?",
+        answer: (
+          <>
+            MetaMask Snaps allows non-EVM (Ethereum Virtual Machine) blockchains, like the XRP Ledger,
+            to integrate with the MetaMask browser wallet at no cost, making it easy and simple for
+            MetaMask users to interact with these networks.
+          </>
+        ),
+      },
+      {
+        question: "How can I install MetaMask Snap for XRPL?",
+        answer: (
+          <>
+            Adding XRPL Snap to MetaMask is now easy with{" "}
+            <a href="https://app.xrplsnap.com">
+              <strong>app.xrplsnap.com</strong>
+            </a>. On the XRPL Snap site, you will be
+            prompted to add the XRPL network on MetaMask. Follow the steps in this{" "}
+            <a href="https://youtu.be/hG9gshvvZ8E?si=np527R6JxLw1msqT">
+              <strong>video</strong>
+            </a> for a straight installation guide.
+          </>
+        ),
+      },
+      {
+        question: "How to add XRP to MetaMask?",
+        answer: (
+          <>
+            Adding XRP, XRP Ledger native’s currency, to MetaMask is now easy with{" "}
+            <a href="https://app.xrplsnap.com">
+              <strong>app.xrplsnap.com</strong>
+            </a>. On the XRPL Snap site, you will be
+            prompted to add the XRPL network on MetaMask. Follow the steps displayed on your MetaMask to
+            Install XRPL. Once installed, an XRPL account will be generated from your MetaMask account
+            mnemonic. To activate; buy or send some XRP. Watch our{" "}
+            <a href="https://youtu.be/hG9gshvvZ8E?si=np527R6JxLw1msqT">
+              <strong>simple step-by-step tutorial</strong>
+            </a>{" "}
+            on how to add XRP to MetaMask.
+          </>
+        ),
+      },
+      {
+        question: "Can I buy Ripple on MetaMask?",
+        answer: (
+          <>
+            No, Ripple is a company. You’re likely referring to XRP, the native cryptocurrency of XRPL,
+            one of the top blockchains in the world. You can buy XRP using our Transak On-Ramp by
+            clicking the ‘Buy’ button on{" "}
+            <a href="https://app.xrplsnap.com">
+              <strong>app.xrplsnap.com</strong>
+            </a>.
+          </>
+        ),
+      },
+      {
+        question: "Does MetaMask support XRPL?",
+        answer: (
+          <>
+            Yes. MetaMask, thanks to our Snap, now supports XRP Ledger, its tokens, and all transaction
+            types. A bridged XRP on the XRPL EVM can also be accessed through MetaMask. Visit{" "}
+            <a href="https://xrplsnap.com">
+              <strong>xrplsnap.com</strong>
+            </a> and add XRP Ledger Snap to MetaMask.
+          </>
+        ),
+      },
+      {
+        question: "How much XRP is enough to activate my account?",
+        answer: (
+          <>
+            On the XRPL blockchain, activating an account requires a 10 XRP non-usable Reserve balance.
+            This reserve is meant to prevent infinite account creation and spam on the network. Creating
+            Trustlines and adding tokens will require an additional 2 XRP per asset. Learn more about{" "}
+            <a href="https://xrpl.org/docs/concepts/accounts/reserves">
+              <strong>XRP Reserves</strong>
+            </a>.
+          </>
+        ),
+      },
+      {
+        question: "How secure is MetaMask Snap with XRPL?",
+        answer: (
+          <>
+            Developed by Peersyst, a proven blockchain industry leader and a key XRPL community
+            contributor, the Snap has been thoroughly audited by Sayfer, ensuring flawless security for
+            an exceptional community. When interacting with the Snap, your XRPL Wallet inherits security
+            from MetaMask, the gold standard of non-custodial wallets, long proved and trusted by over
+            30 million users.
+          </>
+        ),
+      },
 ];
 
 export function Faqs(): JSX.Element {
     return (
-        <div id="faq" className="max-w-4xl flex flex-col py-12 sm:pt-20 sm:pb-56 gap-6 sm:gap-12 px-4 lg:px-0 w-full">
-            <p className="text-3xl font-semibold text-left">FAQ</p>
-
-            <div className="flex flex-col gap-3">
-                {faqs.map((faq, index) => (
-                    <div key={`faq-${index}`} className="bg-[#2B2B2E] rounded-xl p-8">
-                        <h3 className="font-semibold text-lg">{faq.question}</h3>
-                        <p className="text-sm text-[#B0B0BA]">{faq.answer}</p>
-                    </div>
-                ))}
+      <div
+        id="faq"
+        className="max-w-4xl flex flex-col py-12 sm:pt-20 sm:pb-56 gap-6 sm:gap-12 px-4 lg:px-0 w-full"
+      >
+        <p className="text-3xl font-semibold text-left">FAQ</p>
+  
+        <div className="flex flex-col gap-3">
+          {faqs.map((faq, index) => (
+            <div key={`faq-${index}`} className="bg-[#2B2B2E] rounded-xl p-8">
+              <h3 className="font-semibold text-lg">{faq.question}</h3>
+              <p className="text-sm text-[#B0B0BA]">{faq.answer}</p>
             </div>
+          ))}
         </div>
+      </div>
     );
-}
+  }
+  
